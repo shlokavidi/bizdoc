988da4ce220210e5ca96e1f996e523a12ff083cf
shlokavidi <shlokavg@example.com>
2025-04-05 16:50:53 +0530

    wip: change prompt to extract only company name (and compare from list)

--- key-value-extract.py
+++ key-value-extract.py
@@ -11,13 +11,16 @@ def extract_text_from_pdf(pdf_path):
     return full_text
 
 def extract_key_value_pairs(text):
-    openai.api_key = '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'
+    text = 'PO#987654 2025-04-05 ShipTo:MCD Prim,456ElmSt,Springfield,IL62701 Item#12345 TORANI SYRUP VANILLA Qty:20 Price:22.15'
+    company_names = ['MCDONALD PRIMARY', 'KEHE FOOD DISTRIBUTORS INC', 'BEN E. KEITH CO.', 'MCDONALD SECONDARY', 'BIRITE FOODSERVICE DISTRIBUTORS']
+    openai.api_key = 'token'
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": f''''Extract PO Number, PO Date, Ship To Name, Ship To Adress, Ship To City, Ship To State, Ship to Zip, Item Number, Item Description, Item Quantity, Item Price in json format. sample outputs are given in annotations.json file.
-            \n\n{text}'''}
+            # {"role": "user", "content": f''''Extract PO Number, PO Date, Ship To Name, Ship To Adress, Ship To City, Ship To State, Ship to Zip, Item Number, Item Description, Item Quantity, Item Price in json format. sample outputs are given in annotations.json file.
+            # \n\n{text}'''}
+            {"role": "user", "content": f'''Which company (customer) gave this PO to TORANI? Give in json format just the company name - "COMPANY_NAME"{text}. Map the company name to one of these names: {company_names}'''}
         ],
         max_tokens=500,
         temperature=0.5,
@@ -25,9 +28,16 @@ def extract_key_value_pairs(text):
     return response.choices[0].message['content'].strip()
 
 # Example Usage
-pdf_path = "BEKPO854241.pdf"
+
+# pdf_path = "BEKPO854241.pdf"
 # pdf_path = "edi_po_2885865.pdf"
 # pdf_path = "305332_PO.pdf"
+# pdf_path = "PO#-219337.pdf"
+pdf_path = "Mcdonald_po_type1.pdf"
+pdf_path = "Mcdonald_po_type2.pdf"
+pdf_path = "Mcdonald_po_type3.pdf"
+pdf_path = "Mcdonald_po_type4.pdf"
+pdf_path = "Mcdonald_po_type5.pdf"
 text = extract_text_from_pdf(pdf_path)
 key_value_pairs = extract_key_value_pairs(text)
 print(key_value_pairs)
