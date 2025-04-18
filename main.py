f1887e0dd18a0c40a032f1dbf0ee58509cf44c43
Gridharan Vidi <55907981+gri2vidi@users.noreply.github.com>
2025-04-19 01:31:13 +0530

    Update main.py with streamlit code

--- main.py
+++ main.py
@@ -3,6 +3,33 @@ from src.utils.openai_utils import extract_company_name, extract_po_details, ext
 from src.utils.pdf_utils import extract_text_from_pdf
 from src.utils.db_utils import insert_po_details
 from app import display_items, app
+import pandas as pd
+import streamlit as st
+
+
+def get_file_name():
+    file_name = st.text_input("Enter the file name", value="keith-BEKPO125241-1.pdf")
+    return file_name
+
+
+def print_in_ui(line_items, po_number, po_dates, company_name):
+    df_line_items = pd.DataFrame(line_items)
+    df_line_items['PO Number'] = po_number
+    df_line_items['PO Date'] = po_dates
+    df_line_items['Company Name'] = company_name
+    df_line_items['Line Item'] = df_line_items.index + 1
+    df_line_items = df_line_items[['Company Name', 'PO Number', 'PO Date', 'Line Item'] + [col for col in df_line_items.columns if col not in ['Company Name', 'PO Number', 'PO Date', 'Line Item']]]
+    # print(f'{df_line_items}')
+
+    # Streamlit app to display the dataframe
+    st.title("Purchase Order Details")
+    st.write("Below are the extracted details from the PDF:")
+
+    # Display the dataframe in Streamlit
+    st.dataframe(df_line_items)
+
+
+
 # Example Usage
 
 # pdf_path = "pdf-samples/BEKPO854241.pdf"
@@ -22,24 +49,31 @@ from app import display_items, app
 # pdf_path = "pdf-samples/PURORD-3.pdf"
 # pdf_path = "pdf-samples/TORANI - EPICUREAN PO #A62460, #A62470, #A62480.pdf"
 # pdf_path = "pdf-samples/sysco-LA-po-44802350.pdf"
-file_name = input("Enter the file name: ")
-pdf_path = f"pdf-samples/{file_name}"
+# file_name = input("Enter the file name (default: pdf-data/keith-BEKPO125241-1.pdf): ") or "keith-BEKPO125241-1.pdf"
+file_name = get_file_name()
+pdf_path = f"pdf-data/{file_name}"
 text = extract_text_from_pdf(pdf_path)
 company_name = extract_company_name(text)
 po_number, po_dates = extract_po_details(text, company_name)
 line_items = extract_line_items(text, company_name)
 
-# insert_po_details(company_name, po_numbers, po_dates, line_items)
+# insert_po_details(company_name, po_number, po_dates, line_items)
 print(f'PO number is {po_number}')
-items = display_items(po_number)
+# print(line_items)
+# Copy the line_items in to dataframe
+
+print_in_ui(line_items, po_number, po_dates, company_name)
+
+
+# items = display_items(po_number)
 # print(json.dumps(items, indent=4))
 # print(f"PO Numbers: {po_numbers}")
 # print(f"PO Dates: {po_dates}")
 # print(f"Line Items: {line_items}")
 
 
-'''
-1. file name as argument
-2. line items
-3. insert in table
-'''
+# '''
+# 1. file name as argument
+# 2. line items
+# 3. insert in table
+# '''
